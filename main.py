d5daf36c781836fdf6d15f4409ee7fc3c0ffbe94
Minh <ntlminh27@gmail.com>
2025-04-10 07:55:48 +0700

    Try a new sound-playing library

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import random
-from playsound import playsound
+import pygame
+import time
 from TTS.api import TTS
 
-words = ["hello", "world", "Vietnam", "example"]
+words = ["hello", "world", "Vietnam", "example", "on track", "schedule", "Thuy"]
+
 random.shuffle(words)
 
-tts = TTS(model_name="tts_models/en/ljspeech/glow-tts")
+pygame.mixer.init()
+
+tts = TTS(model_name="tts_models/en/ljspeech/vits")
 # tts = TTS(model_name="tts_models/en/ljspeech/vits", phonemizer="gruut")
 # glow-tts
 
 for word in words:
-    tts.tts_to_file(text=word, file_path=f"files/{word}.wav")
-    playsound(f"files/{word}.wav")
+    file_path = f"files/{word}.wav"
+    tts.tts_to_file(text=word, file_path=file_path)
+
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():  
+        try:
+            
+            input()
+
+        except KeyboardInterrupt:  
+            pygame.mixer.music.stop()
+            print("Stopped.")
+            break
+
+
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+
+pygame.mixer.quit()
+# pygame.quit()
